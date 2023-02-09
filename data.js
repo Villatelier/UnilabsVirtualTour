dc171ddfb692184117e410bc035e3a9040d311ae
Villatelier <124809163+Villatelier@users.noreply.github.com>
2023-02-09 21:54:34 +0000

    update

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-unilabs_pv360_entry",
+      "id": "0-entry",
       "name": "Entry",
       "levels": [
         {
@@ -24,22 +24,22 @@ var APP_DATA = {
       ],
       "faceSize": 1500,
       "initialViewParameters": {
-        "yaw": 0.016456636800006308,
+        "yaw": 0,
         "pitch": 0,
-        "fov": 1.0989799328023968
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 0,
-          "pitch": 0,
+          "yaw": 0.014450311276355876,
+          "pitch": 0.11377685634057677,
           "rotation": 0,
-          "target": "1-unilabs_pv360_reception"
+          "target": "1-reception"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-unilabs_pv360_reception",
+      "id": "1-reception",
       "name": "Reception",
       "levels": [
         {
@@ -68,34 +68,34 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.859451109129454,
-          "pitch": 0.12011329647965319,
+          "yaw": 1.5845945185045238,
+          "pitch": 0.1814854198482312,
           "rotation": 0,
-          "target": "0-unilabs_pv360_entry"
+          "target": "2-waiting-lounge"
         },
         {
-          "yaw": 1.5772217208925507,
-          "pitch": 0.18254141420991488,
+          "yaw": 1.043820743622737,
+          "pitch": 0.1693003610546313,
           "rotation": 0,
-          "target": "2-unilabs_pv360_waiting_lounge"
+          "target": "3-kids-area"
         },
         {
-          "yaw": 1.05123223244928,
-          "pitch": 0.17686618234475837,
+          "yaw": 0.73283114147738,
+          "pitch": 0.24360716102319913,
           "rotation": 0,
-          "target": "3-unilabs_pv360_kids_area"
+          "target": "4-waiting-area1"
         },
         {
-          "yaw": 0.7725507462667025,
-          "pitch": 0.24470379091708594,
+          "yaw": 2.624793298477127,
+          "pitch": 0.16282862254761454,
           "rotation": 0,
-          "target": "4-unilabs_pv360_wainting_area1"
+          "target": "0-entry"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-unilabs_pv360_waiting_lounge",
+      "id": "2-waiting-lounge",
       "name": "Waiting Lounge",
       "levels": [
         {
@@ -124,34 +124,34 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.7830676441324087,
-          "pitch": 0.3348881505729917,
+          "yaw": -1.7804673560206918,
+          "pitch": 0.3496551762769631,
           "rotation": 0,
-          "target": "3-unilabs_pv360_kids_area"
+          "target": "3-kids-area"
         },
         {
-          "yaw": -2.377994276597766,
-          "pitch": 0.2778631868692276,
+          "yaw": -2.384003157325761,
+          "pitch": 0.2680573874034291,
           "rotation": 0,
-          "target": "4-unilabs_pv360_wainting_area1"
+          "target": "4-waiting-area1"
         },
         {
-          "yaw": -3.1218019601105365,
-          "pitch": 0.2053644048806742,
+          "yaw": -3.1388365138285774,
+          "pitch": 0.22352876827873303,
           "rotation": 0,
-          "target": "1-unilabs_pv360_reception"
+          "target": "1-reception"
         },
         {
-          "yaw": 2.791350918094672,
-          "pitch": 0.07058108470170943,
+          "yaw": 2.7848212570830118,
+          "pitch": 0.06851783198605332,
           "rotation": 0,
-          "target": "0-unilabs_pv360_entry"
+          "target": "0-entry"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-unilabs_pv360_kids_area",
+      "id": "3-kids-area",
       "name": "Kids Area",
       "levels": [
         {
@@ -180,23 +180,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.7428360042937854,
-          "pitch": 0.30031287984858324,
+          "yaw": -2.749698317082421,
+          "pitch": 0.27767352069921003,
           "rotation": 0,
-          "target": "2-unilabs_pv360_waiting_lounge"
+          "target": "2-waiting-lounge"
         },
         {
-          "yaw": -2.228852509597534,
-          "pitch": 0.16645388300672082,
+          "yaw": -2.2263268869218518,
+          "pitch": 0.2005686417908361,
           "rotation": 0,
-          "target": "1-unilabs_pv360_reception"
+          "target": "1-reception"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-unilabs_pv360_wainting_area1",
-      "name": "Wainting Area1",
+      "id": "4-waiting-area1",
+      "name": "Waiting Area1",
       "levels": [
         {
           "tileSize": 256,
@@ -224,40 +224,46 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.021366212769081017,
-          "pitch": 0.09528903426962287,
+          "yaw": -1.490874459647893,
+          "pitch": 0.20105642676868207,
           "rotation": 0,
-          "target": "5-unilabs_pv360_waiting_area2"
+          "target": "1-reception"
         },
         {
-          "yaw": -0.210479581089686,
-          "pitch": 0.042868834443694936,
+          "yaw": -0.04511704178382914,
+          "pitch": 0.09088934735882859,
           "rotation": 0,
-          "target": "13-unilabs_pv360_cardiology_room"
+          "target": "5-waiting-area2"
         },
         {
-          "yaw": 0.11315869831507719,
-          "pitch": 0.049131415706950676,
+          "yaw": -0.2249685565304702,
+          "pitch": 0.041589327692848954,
           "rotation": 0,
-          "target": "12-unilabs_pv360_radiology_room"
+          "target": "12-cardiology-room"
         },
         {
-          "yaw": 1.8960898941955815,
-          "pitch": 0.1682261041610289,
+          "yaw": 0.0959139238557114,
+          "pitch": 0.03629773237832623,
           "rotation": 0,
-          "target": "6-unilabs_pv360_corridor"
+          "target": "11-radiology-room"
         },
         {
-          "yaw": -2.9040663429807605,
-          "pitch": 0.281443901445277,
+          "yaw": 1.8843401070971506,
+          "pitch": 0.10498163963392138,
           "rotation": 0,
-          "target": "11-unilabs_pv360_staff_relax_room"
+          "target": "6-corridor"
+        },
+        {
+          "yaw": -2.844658939716995,
+          "pitch": 0.16972995147167147,
+          "rotation": 0,
+          "target": "14-staff-kitchen"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-unilabs_pv360_waiting_area2",
+      "id": "5-waiting-area2",
       "name": "Waiting Area2",
       "levels": [
         {
@@ -286,52 +292,52 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.08861932219501156,
-          "pitch": 0.0904962024845517,
+          "yaw": 0.06514080197841565,
+          "pitch": 0.0873185941427419,
           "rotation": 0,
-          "target": "4-unilabs_pv360_wainting_area1"
+          "target": "4-waiting-area1"
         },
         {
-          "yaw": 0.21589999779934388,
-          "pitch": 0.0296535137639804,
+          "yaw": -0.08080274024026401,
+          "pitch": 0.06157922578869979,
           "rotation": 0,
-          "target": "15-unilabs_pv360_staff_kitchen"
+          "target": "6-corridor"
         },
         {
-          "yaw": -0.08638703115466484,
-          "pitch": 0.04255390154461125,
+          "yaw": 0.2137528603674692,
+          "pitch": 0.0538045711064683,
           "rotation": 0,
-          "target": "6-unilabs_pv360_corridor"
+          "target": "14-staff-kitchen"
         },
         {
-          "yaw": -1.617258798941977,
-          "pitch": 0.0527335883286959,
+          "yaw": 1.7370756850600415,
+          "pitch": 0.07523423340954061,
           "rotation": 0,
-          "target": "10-unilabs_pv360_mri_room"
+          "target": "13-recovery-room"
         },
         {
-          "yaw": -2.3374946622231327,
-          "pitch": 0.24895279238588763,
+          "yaw": 2.1037918696658586,
+          "pitch": 0.18171362488280884,
           "rotation": 0,
-          "target": "12-unilabs_pv360_radiology_room"
+          "target": "12-cardiology-room"
         },
         {
-          "yaw": 2.09645205658856,
-          "pitch": 0.16011770944578174,
+          "yaw": -2.389921439351429,
+          "pitch": 0.21648329177623538,
           "rotation": 0,
-          "target": "13-unilabs_pv360_cardiology_room"
+          "target": "11-radiology-room"
         },
         {
-          "yaw": 1.7392211066516712,
-          "pitch": 0.05118430538561647,
+          "yaw": -1.6443761234670173,
+          "pitch": 0.04190197528344619,
           "rotation": 0,
-          "target": "14-unilabs_pv360_recovery_room"
+          "target": "10-mri-room"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-unilabs_pv360_corridor",
+      "id": "6-corridor",
       "name": "Corridor",
       "levels": [
         {
@@ -360,52 +366,52 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.012371164095499765,
-          "pitch": 0.04832113381401726,
+          "yaw": 0.9112335447857767,
+          "pitch": 0.2812323850256071,
           "rotation": 0,
-          "target": "9-unilabs_pv360_kids_collection"
+          "target": "8-sperm-collection"
         },
         {
-          "yaw": 0.3091746307526506,
-          "pitch": 0.14468987480486462,
+          "yaw": 0.34855113766507273,
+          "pitch": 0.12621323734724754,
           "rotation": 0,
-          "target": "7-unilabs_pv360_blood_collection"
+          "target": "7-blood-collection"
         },
         {
-          "yaw": 0.8772700316865389,
-          "pitch": 0.32239850563094663,
+          "yaw": 0.02292681295747201,
+          "pitch": 0.04018228129213419,
           "rotation": 0,
-          "target": "8-unilabs_pv360_sperm_collection"
+          "target": "9-kids-collection"
         },
         {
-          "yaw": -1.7173962947897827,
-          "pitch": 0.06145669749677651,
+          "yaw": -1.720523565337178,
+          "pitch": 0.046819097091843176,
           "rotation": 0,
-          "target": "5-unilabs_pv360_waiting_area2"
+          "target": "5-waiting-area2"
         },
         {
-          "yaw": -2.268855428352092,
-          "pitch": 0.39217285743465524,
+          "yaw": -2.312953848824117,
+          "pitch": 0.2993334007202808,
           "rotation": 0,
-          "target": "4-unilabs_pv360_wainting_area1"
+          "target": "4-waiting-area1"
         },
         {
-          "yaw": -1.8619085668081166,
-          "pitch": 0.03719622628512553,
+          "yaw": -1.8432230111463888,
+          "pitch": 0.02552235821015003,
           "rotation": 0,
-          "target": "13-unilabs_pv360_cardiology_room"
+          "target": "12-cardiology-room"
         },
         {
-          "yaw": -1.6131125845825594,
-          "pitch": 0.041694964293915504,
+          "yaw": -1.602337748644569,
+          "pitch": 0.028485359543246602,
           "rotation": 0,
-          "target": "12-unilabs_pv360_radiology_room"
+          "target": "11-radiology-room"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-unilabs_pv360_blood_collection",
+      "id": "7-blood-collection",
       "name": "Blood Collection",
       "levels": [
         {
@@ -434,16 +440,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -3.1023888562181217,
-          "pitch": 0.3257863480203689,
+          "yaw": -3.138524666487811,
+          "pitch": 0.31514141325921585,
           "rotation": 0,
-          "target": "6-unilabs_pv360_corridor"
+          "target": "6-corridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-unilabs_pv360_sperm_collection",
+      "id": "8-sperm-collection",
       "name": "Sperm Collection",
       "levels": [
         {
@@ -472,16 +478,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.956779343153613,
-          "pitch": 0.3319709415786427,
+          "yaw": -2.8801181561054996,
+          "pitch": 0.3339459663124291,
           "rotation": 0,
-          "target": "6-unilabs_pv360_corridor"
+          "target": "6-corridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "9-unilabs_pv360_kids_collection",
+      "id": "9-kids-collection",
       "name": "Kids Collection",
       "levels": [
         {
@@ -510,16 +516,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.605242299902341,
-          "pitch": 0.27169177676306333,
+          "yaw": 2.6192613404406213,
+          "pitch": 0.25143047157611953,
           "rotation": 0,
-          "target": "6-unilabs_pv360_corridor"
+          "target": "6-corridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "10-unilabs_pv360_mri_room",
+      "id": "10-mri-room",
       "name": "MRI Room",
       "levels": [
         {
@@ -548,60 +554,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.1318653348293886,
-          "pitch": 0.1815131381175128,
+          "yaw": -2.0829441200711862,
+          "pitch": 0.1696189589454562,
           "rotation": 0,
-          "target": "5-unilabs_pv360_waiting_area2"
+          "target": "5-waiting-area2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "11-unilabs_pv360_staff_relax_room",
-      "name": "Staff Relax Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1500,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.7655856658733295,
-          "pitch": 0.06587937274809441,
-          "rotation": 0,
-          "target": "4-unilabs_pv360_wainting_area1"
-        },
-        {
-          "yaw": -3.0306477846365034,
-          "pitch": 0.10605977020892432,
-          "rotation": 0,
-          "target": "15-unilabs_pv360_staff_kitchen"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "12-unilabs_pv360_radiology_room",
+      "id": "11-radiology-room",
       "name": "Radiology Room",
       "levels": [
         {
@@ -630,16 +592,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.6765218044337473,
-          "pitch": 0.10415376066391602,
+          "yaw": -2.661716873310896,
+          "pitch": 0.092653717948604,
           "rotation": 0,
-          "target": "5-unilabs_pv360_waiting_area2"
+          "target": "5-waiting-area2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "13-unilabs_pv360_cardiology_room",
+      "id": "12-cardiology-room",
       "name": "Cardiology Room",
       "levels": [
         {
@@ -668,16 +630,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.849000089357064,
-          "pitch": 0.11262400189469801,
+          "yaw": -2.8204674130301868,
+          "pitch": 0.09800084140088394,
           "rotation": 0,
-          "target": "5-unilabs_pv360_waiting_area2"
+          "target": "5-waiting-area2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "14-unilabs_pv360_recovery_room",
+      "id": "13-recovery-room",
       "name": "Recovery Room",
       "levels": [
         {
@@ -706,16 +668,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.9264951259891987,
-          "pitch": 0.16398088939774347,
+          "yaw": 1.969324152185088,
+          "pitch": 0.15409079169896778,
           "rotation": 0,
-          "target": "5-unilabs_pv360_waiting_area2"
+          "target": "5-waiting-area2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "15-unilabs_pv360_staff_kitchen",
+      "id": "14-staff-kitchen",
       "name": "Staff Kitchen",
       "levels": [
         {
@@ -744,22 +706,28 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.0637945438991352,
-          "pitch": 0.19252478614701296,
+          "yaw": 1.1020733837243668,
+          "pitch": 0.1444139443300294,
           "rotation": 0,
-          "target": "16-unilabs_pv360_staff_room"
+          "target": "15-staff-room"
         },
         {
-          "yaw": -2.667877042199443,
-          "pitch": 0.16502963156504435,
+          "yaw": -2.6717327568291385,
+          "pitch": 0.13685866582675388,
           "rotation": 0,
-          "target": "11-unilabs_pv360_staff_relax_room"
+          "target": "16-staff-relax-room"
+        },
+        {
+          "yaw": -2.86921613811524,
+          "pitch": 0.12465978772592834,
+          "rotation": 0,
+          "target": "4-waiting-area1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "16-unilabs_pv360_staff_room",
+      "id": "15-staff-room",
       "name": "Staff Room",
       "levels": [
         {
@@ -788,16 +756,66 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.615682269517574,
-          "pitch": 0.20482167700014742,
+          "yaw": -1.6586276070916028,
+          "pitch": 0.12788843427174612,
           "rotation": 0,
-          "target": "15-unilabs_pv360_staff_kitchen"
+          "target": "14-staff-kitchen"
         },
         {
-          "yaw": -1.9357732708439208,
-          "pitch": 0.05077748357246392,
+          "yaw": -1.9239065080768256,
+          "pitch": 0.07815751658344183,
           "rotation": 0,
-          "target": "11-unilabs_pv360_staff_relax_room"
+          "target": "16-staff-relax-room"
+        },
+        {
+          "yaw": -2.0235387881945393,
+          "pitch": 0.0749161471720825,
+          "rotation": 0,
+          "target": "4-waiting-area1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-staff-relax-room",
+      "name": "Staff Relax Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7855885608855964,
+          "pitch": 0.05524152980291319,
+          "rotation": 0,
+          "target": "4-waiting-area1"
+        },
+        {
+          "yaw": -3.0477937490196574,
+          "pitch": 0.06779608695775075,
+          "rotation": 0,
+          "target": "14-staff-kitchen"
         }
       ],
       "infoHotspots": []
